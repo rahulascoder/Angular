029704f2fcb8286ac26419df50508745b13acee0
rahulascoder <rahulascoder85@gmail.com>
2025-03-06 10:57:31 +0530

    [Rahul Kumar] Modifying Single Page Application using AngularJS �

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -1,23 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,  // Marking the component as standalone
+  imports: [RouterOutlet, FormsModule],  // Import necessary modules here
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'HelloWOrld';
+  title = 'Helloworld';
   imgUrl = "/assets/BL_logo.jpg";
+  userName: string = "";
+  nameError:string="";
   url = "https://www.bridgelabz.com";
-  userName:String = "";
- 
+
   ngOnInit(): void {
-    this.title = "Hello from Bridgelabz..";
+    this.title = "Hello from BridgeLabz.";
   }
-  onClick(event:MouseEvent){
-    console.log("Save button is clicked !", event);
+
+  onClick($event: MouseEvent): void {
+    console.log("Save button is clicked", $event);
     window.open(this.url, "_blank");
+  }
+
+  onInput($event:any){
+    console.log("Change event occurred",$event.data);
+    const nameRegex=RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError="";
+      return;
+    }
+    this.nameError="Name is Incorrect";
   }
 }
--- HelloWorld/src/app/app.module.ts
+++ HelloWorld/src/app/app.module.ts
@@ -1,19 +1,17 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import {AppRoutingModule} from './app-routing.module';
-import { FormsModule } from "@angular/forms";
-import { AppComponent } from "./app.component";
+// import { BrowserModule } from "@angular/platform-browser";
+// import { NgModule } from "@angular/core";
+// import {AppRoutingModule} from './app-routing.module';
+// import { FormsModule } from "@angular/forms";
+// import { AppComponent } from "./app.component";
 
-@NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        AppRoutingModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
-})
-export class AppModule{}
+// @NgModule({
+//     declarations: [AppComponent ],
+//     imports: [
+//         BrowserModule,
+//         FormsModule,
+//         AppRoutingModule
+//     ],
+//     // providers: [],
+//     bootstrap: [AppComponent]
+// })
+// export class AppModule{}
